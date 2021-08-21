9d3ea42927c0b7808fd690d496f749bec5cb00b5
hanni <a2370225285@gmail.com>
2021-08-22 00:29:28 +0800

    change test.c

--- test.c
+++ test.c
@@ -4,5 +4,6 @@
 int main(int argc, char *argv[])
 {
 	if(argc) printf("num:%d,content:%d,%c\n",argc,atoi(argv[1]),&argv[1]);
+	if(argc) printf("num:%d,content:%d,%c\n",argc,atoi(argv[2]),&argv[2]);
 	return 0;
 }
